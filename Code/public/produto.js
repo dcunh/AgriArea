4d7e9bc81b14b019ae51f75102dbca9a72cbfe85
AC_Marvel <gil_1_10@hotmail.com>
2020-12-13 12:11:08 +0000

    Duvidas

---
+++ Code/public/produto.js
@@ -0,0 +1,24 @@
+
+window.onload = async function() {
+    let idProduto = sessionStorage.getItem("idproduto");
+
+    let produto = await $.ajax({
+        url: "/api/produto/"+idProduto,
+        method: "get",
+        dataType: "json"
+    });
+    console.log(produto);
+
+    if (produto.fertelizante != null){
+        document.getElementById("fertilizante").src = produto.fertilizante;
+    }
+
+    document.getElementById("Nome").innerHTML = produto.Nome;
+    document.getElementById("nome").innerHTML = album.Nome;
+
+    let html = "";
+    for (let info of produto.infos){
+        html +="<p>"+info.Nome+" - "+info.Preco+" €</P>";
+    }
+    document.getElementById("infos").innerHTML = html
+}
